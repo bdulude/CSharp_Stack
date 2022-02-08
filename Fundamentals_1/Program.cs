d5e4188bf6fbac233e201aae1297987babb6662d
bdulude <bdulude@gmail.com>
2022-02-07 19:13:22 -0800

    Fundamentals 1

---
+++ Fundamentals_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fundamentals_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x < 256; x++)
+            {
+                Console.WriteLine(x);
+            }
+            for (int y = 1; y < 101; y++)
+            {
+                if (y % 3 == 0 && y % 5 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (y % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (y % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else {
+                    Console.WriteLine(y);
+                }
+            }
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
