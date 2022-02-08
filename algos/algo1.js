2b849f41b553d0a5a4146d7ed344556c5f18080f
bdulude <bdulude@gmail.com>
2022-02-07 19:04:01 -0800

    update

--- algos/algo1.js
+++ algos/algo1.js
@@ -3,122 +3,122 @@
  * linked to other Node instances to form a list of linked nodes.
  */
 class Node {
-  /**
-   * Constructs a new Node instance. Executed when the 'new' keyword is used.
-   * @param {any} data The data to be added into this new instance of a Node.
-   *    The data can be anything, just like an array can contain strings,
-   *    numbers, objects, etc.
-   * @returns {Node} A new Node instance is returned automatically without
-   *    having to be explicitly written (implicit return).
-   */
-  constructor(data) {
-    this.data = data;
     /**
-     * This property is used to link this node to whichever node is next
-     * in the list. By default, this new node is not linked to any other
-     * nodes, so the setting / updating of this property will happen sometime
-     * after this node is created.
+     * Constructs a new Node instance. Executed when the 'new' keyword is used.
+     * @param {any} data The data to be added into this new instance of a Node.
+     *    The data can be anything, just like an array can contain strings,
+     *    numbers, objects, etc.
+     * @returns {Node} A new Node instance is returned automatically without
+     *    having to be explicitly written (implicit return).
      */
-    this.next = null;
-  }
-}
-
-/**
- * Class to represent a list of linked nodes. Nodes CAN be linked together
- * without this class to form a linked list, but this class helps by providing
- * a place to keep track of the start node (head) of the list at all times and
- * as a place to add methods (functions inside an object) so that every new
- * linked list that is instantiated will inherit helpful the same helpful
- * methods, just like every array you create inherits helpful methods.
- */
-class SinglyLinkedList {
-/**
- * Constructs a new instance of an empty linked list that inherits all the
- * methods.
- * @returns {SinglyLinkedList} The new list that is instantiated is implicitly
- *    returned without having to explicitly write "return".
- */
-constructor() {
-    this.head = null;
-}
-
-/**
- * Determines if this list is empty.
- * - Time: O(?).
- * - Space: O(?).
- * @returns {boolean}
- */
-isEmpty() {
-    if (this.head == null){
-        return true
+    constructor(data) {
+        this.data = data;
+        /**
+        * This property is used to link this node to whichever node is next
+        * in the list. By default, this new node is not linked to any other
+        * nodes, so the setting / updating of this property will happen sometime
+        * after this node is created.
+        */
+        this.next = null;
     }
-    return false
 }
 
 /**
- * Creates a new node with the given data and inserts it at the back of
- * this list.
- * - Time: O(?).
- * - Space: O(?).
- * @param {any} data The data to be added to the new node.
- * @returns {SinglyLinkedList} This list.
- */
+* Class to represent a list of linked nodes. Nodes CAN be linked together
+* without this class to form a linked list, but this class helps by providing
+* a place to keep track of the start node (head) of the list at all times and
+* as a place to add methods (functions inside an object) so that every new
+* linked list that is instantiated will inherit helpful the same helpful
+* methods, just like every array you create inherits helpful methods.
+*/
+class SinglyLinkedList {
+    /**
+     * Constructs a new instance of an empty linked list that inherits all the
+     * methods.
+     * @returns {SinglyLinkedList} The new list that is instantiated is implicitly
+     *    returned without having to explicitly write "return".
+     */
+    constructor() {
+        this.head = null;
+    }
 
+    /**
+     * Determines if this list is empty.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {boolean}
+     */
+    isEmpty() {
+        if (this.head == null) {
+            return true;
+        }
+        return false;
+    }
 
-/*
+    /**
+     * Creates a new node with the given data and inserts it at the back of
+     * this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} data The data to be added to the new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+
+    /*
     create a function that takes in a singly linked list
     iterate through the linked the list to find the end
         where the next value is undefined
     set the next variable equal to the data that was passed in
     return head
-*/
+    */
 
-insertAtBack(data) {
-    if (this.isEmpty() == false){
-        this.head = new Node(data)
-        return this
+    insertAtBack(data) {
+        if (this.isEmpty() == false) {
+            this.head = new Node(data);
+            return this;
+        }
+        let runner = this.head;
+        while (runner.next != null) {
+            runner = runner.next;
+        }
+        runner.next = new Node(data);
+        return this;
     }
-    let runner = this.head
-    while(runner.next != null) {
-        runner = runner.next
+
+    /**
+     * Calls insertAtBack on each item of the given array.
+     * - Time: O(n * m) n = list length, m = arr.length.
+     * - Space: O(1) constant.
+     * @param {Array<any>} vals The data for each new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+
+    seedFromArr(vals) {
+        for (const item of vals) {
+            this.insertAtBack(item);
+        }
+        return this;
     }
-    runner.next = new Node(data)
-    return this
+
+    /**
+     * Converts this list into an array containing the data of each node.
+     * - Time: O(n) linear.
+     * - Space: O(n).
+     * @returns {Array<any>} An array of each node's data.
+     */
+    toArr() {
+        const arr = [];
+        let runner = this.head;
+
+        while (runner) {
+            arr.push(runner.data);
+            runner = runner.next;
+        }
+        return arr;
+    }
 }
 
-/**
- * Calls insertAtBack on each item of the given array.
- * - Time: O(n * m) n = list length, m = arr.length.
- * - Space: O(1) constant.
- * @param {Array<any>} vals The data for each new node.
- * @returns {SinglyLinkedList} This list.
- */
 
-
-seedFromArr(vals) {
-    for (const item of vals) {
-    this.insertAtBack(item);
-    }
-    return this;
-}
-
-/**
- * Converts this list into an array containing the data of each node.
- * - Time: O(n) linear.
- * - Space: O(n).
- * @returns {Array<any>} An array of each node's data.
- */
-toArr() {
-    const arr = [];
-    let runner = this.head;
-
-    while (runner) {
-    arr.push(runner.data);
-    runner = runner.next;
-    }
-    return arr;
-}
-}
 
 const emptyList = new SinglyLinkedList();
 
@@ -127,7 +127,7 @@ const biNodeList = new SinglyLinkedList().seedFromArr([1, 2]);
 const firstThreeList = new SinglyLinkedList().seedFromArr([1, 2, 3]);
 const secondThreeList = new SinglyLinkedList().seedFromArr([4, 5, 6]);
 const unorderedList = new SinglyLinkedList().seedFromArr([
-  -5, -10, 4, -3, 6, 1, -7, -2,
+    -5, -10, 4, -3, 6, 1, -7, -2,
 ]);
 
 // node 4 connects to node 1, back to head
@@ -139,7 +139,7 @@ const loopList = new SinglyLinkedList().seedFromArr([1, 2, 3, 4]);
 // loopList.head.next.next.next = loopList.head.next;
 
 const sortedDupeList = new SinglyLinkedList().seedFromArr([
-  1, 1, 1, 2, 3, 3, 4, 5, 5,
+    1, 1, 1, 2, 3, 3, 4, 5, 5,
 ]);
 
 console.log(singleNodeList.toArr());
---
+++ initialize_projects.md
@@ -0,0 +1,19 @@
+## C# Project Initialization 
+<br>
+
+1. >dotnet new console -o ProjectName
+2. >cd ProjectName
+3. >code .
+4. >dotnet run
+
+<br>
+<br>
+
+**See all templates:**
+>dotnet new
+
+<br>
+<br>
+
+**New gitignore**
+>dotnet new gitignore
