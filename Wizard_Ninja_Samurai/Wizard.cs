663637353e976d56683c9b0bf3d76db1a5130962
bdulude <bdulude@gmail.com>
2022-02-09 12:41:18 -0800

    Wizard, Ninja, and Samurai

---
+++ Wizard_Ninja_Samurai/Wizard.cs
@@ -0,0 +1,13 @@
+namespace Wizard_Ninja_Samurai
+{
+    class Wizard : Human
+    {
+        public Wizard(string name)
+        {
+            Name = name;
+            Strength = 1;
+            Intelligence = 6;
+            Dexterity = 3;
+        }
+    }
+}
