e00e157015cb1d9f69d3c2e31e03a6a178f96a88
bdulude <bdulude@gmail.com>
2022-02-14 23:22:18 -0800

    Finalized

--- Passcode_Generator/Controllers/HomeController.cs
+++ Passcode_Generator/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Passcode_Generator.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace Passcode_Generator.Controllers
 {
@@ -18,15 +19,28 @@ namespace Passcode_Generator.Controllers
             _logger = logger;
         }
 
+        [HttpGet("")]
         public IActionResult Index()
         {
+            int? number = HttpContext.Session.GetInt32("number");
+            HttpContext.Session.SetInt32("number", number == null ? default(int) + 1 : number.Value + 1);
+            ViewBag.number = HttpContext.Session.GetInt32("number");
+            ViewBag.passcode = GeneratePW();
             return View();
         }
 
-        public IActionResult Privacy()
+        private string GeneratePW()
         {
-            return View();
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~! @#$%^&*";
+            Random rand = new Random();
+            String output = "";
+            for (int x = 0; x < 14; x++)
+            {
+                output = output + chars[rand.Next(0,72)];
+            }
+            return output;
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
