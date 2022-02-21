0314db6a9ca1c34c997ff55cc4f94fafc8ca9919
bdulude <bdulude@gmail.com>
2022-02-20 21:06:44 -0800

    finalized

--- CRUDelicious/Controllers/HomeController.cs
+++ CRUDelicious/Controllers/HomeController.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using CRUDelicious.Models;
+
 
 namespace CRUDelicious.Controllers
 {
@@ -13,14 +15,97 @@ namespace CRUDelicious.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private CRUDeliciousContext db;
+
+        public HomeController(CRUDeliciousContext context)
         {
-            _logger = logger;
+            db = context;
         }
 
+        [HttpGet("")]
         public IActionResult Index()
         {
+            ViewBag.AllDishes = db.Dishes.OrderByDescending(d => d.CreatedAt);
             return View();
+        }
+
+        [HttpGet("new")]
+        public IActionResult New()
+        {
+            return View("new");
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateDish(Dish newDish)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Dishes.Add(newDish);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else 
+            {
+                return View("new");
+            }
+        }
+
+        [HttpGet("/{dishId}")]
+        public IActionResult ReadDish(int dishId)
+        {
+            Dish RetrievedDish = db.Dishes.SingleOrDefault(d => d.DishId == dishId);
+            if (RetrievedDish != null)
+            {
+                ViewBag.RetrievedDish = RetrievedDish;
+                return View("read");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("edit/{dishId}")]
+        public IActionResult EditDish(int dishId)
+        {
+            Dish RetrievedDish = db.Dishes.SingleOrDefault(d => d.DishId == dishId);
+            if (RetrievedDish != null)
+            {
+                ViewBag.RetrievedDish = RetrievedDish;
+                return View("edit");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("update")]
+        public IActionResult UpdateDish(Dish updateDish)
+        {
+            if (ModelState.IsValid)
+            {
+                Dish RetrievedDish = db.Dishes.SingleOrDefault(d => d.DishId == updateDish.DishId);
+                RetrievedDish.Name = updateDish.Name;
+                RetrievedDish.Chef = updateDish.Chef;
+                RetrievedDish.Tastiness = updateDish.Tastiness;
+                RetrievedDish.Calories = updateDish.Calories;
+                RetrievedDish.Description = updateDish.Description;
+                RetrievedDish.UpdatedAt = DateTime.Now;
+                // db.Update(updateDish);
+                db.SaveChanges();
+                return Redirect("/");
+            }
+            else
+            {
+                return View("edit");
+            }
+        }
+
+        [HttpGet("delete/{dishId}")]
+        public IActionResult DeleteDish(int dishId)
+        {
+            Dish RetrievedDish = db.Dishes.SingleOrDefault(d => d.DishId == dishId);
+            if (RetrievedDish != null)
+            {
+                db.Dishes.Remove(RetrievedDish);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
---
+++ CRUDelicious/Models/Dish.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDelicious.Models
+{
+    public class Dish
+    {
+        [Key]
+        public int DishId {get; set;}
+
+        [Required(ErrorMessage = "Required")]
+        public string Name {get; set;}
+
+        [Required(ErrorMessage = "Required")]
+        public string Chef {get; set;}
+
+        [Required(ErrorMessage = "Required")]
+        [Range(1, 5)]
+        public int Tastiness {get; set;}
+
+        [Required(ErrorMessage = "Required")]
+        [Range(0, 50000)]
+        public int Calories {get; set;}
+
+        [Required(ErrorMessage = "Required")]
+        public string Description {get; set;}
+        
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+
+}
