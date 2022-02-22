12b3803ae98a1ba531b72ed1dfedde9b484ce443
bdulude <bdulude@gmail.com>
2022-02-22 11:05:05 -0800

    algo

---
+++ algos/algo10.js
@@ -0,0 +1,220 @@
+/**
+ * Class to represent a Node in a Binary Search Tree (BST).
+ */
+class BSTNode {
+    /**
+     * Constructs a new instance of a BST node.
+     * @param {number} data The integer to store in the node.
+     */
+    constructor(data) {
+        this.data = data;
+        /**
+         * These properties are how this node is connected to other nodes to form
+         * the tree. Similar to .next in a SinglyLinkedList except a BST node can
+         * be connected to two other nodes. To start, new nodes will not be
+         * connected to any other nodes, these properties will be set after
+         * the new node is instantiated.
+         *
+         * @type {BSTNode|null}
+         */
+        this.left = null;
+        /** @type {BSTNode|null} */
+        this.right = null;
+    }
+}
+
+/**
+ * Represents an ordered tree of nodes where the data of left nodes are <= to
+ * their parent and the data of nodes to the right are > their parent's data.
+ */
+class BinarySearchTree {
+    constructor() {
+        /**
+         * Just like the head of a linked list, this is the start of our tree which
+         * branches downward from here.
+         *
+         * @type {BSTNode|null}
+         */
+        this.root = null;
+    }
+
+    /**
+     * Determines if this tree is empty.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {boolean} Indicates if this tree is empty.
+     */
+    isEmpty() {
+        return this.root === null ? true : false;
+    }
+
+    /**
+     * Retrieves the smallest integer data from this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} current The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {number} The smallest integer from this tree.
+     */
+    min(current = this.root) {
+        if (current === null) return null;
+        while (current.left != null) {
+            current = current.left;
+        }
+        return current.data;
+    }
+
+    /**
+     * Retrieves the smallest integer data from this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} current The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {number} The smallest integer from this tree.
+     */
+    minRecursive(current = this.root) { 
+        if (current === null) return null;
+        if (current.left === null) {
+            return current.data;
+        }
+        return this.minRecursive(current.left);
+    }
+
+    /**
+     * Retrieves the largest integer data from this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} current The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {number} The largest integer from this tree.
+     */
+    max(current = this.root) { 
+        if (current === null) return null;
+        while (current.right != null) {
+            current = current.right;
+        }
+        return current.data;
+    }
+
+    /**
+     * Retrieves the largest integer data from this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} current The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {number} The largest integer from this tree.
+     */
+    maxRecursive(current = this.root) { 
+        if (current === null) return null;
+        if (current.right === null) {
+            return current.data;
+        }
+        return this.maxRecursive(current.right);
+    }
+
+    /**
+     * Inserts a new node with the given newVal in the right place to preserver
+     * the order of this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {number} newVal The data to be added to a new node.
+     * @returns {BinarySearchTree} This tree.
+     */
+    insert(newVal) {
+        if (this.isEmpty()) {
+            this.root = new BSTNode(newVal);
+            return this;
+        } 
+        let search = true;
+        let runner = this.root;
+        while (search) {
+            if (runner.data <= newVal) {
+                runner = runner.right
+            }
+            else if (runner.data > newVal) {
+                runner = runner.left;
+            }
+        }
+    }
+
+    /**
+     * Inserts a new node with the given newVal in the right place to preserver
+     * the order of this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {number} newVal The data to be added to a new node.
+     * @param {Node} curr The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {BinarySearchTree} This tree.
+     */
+    insertRecursive(newVal, curr = this.root) {}
+
+
+    // Logs this tree horizontally with the root on the left.
+    print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
+        if (!node) {
+            return;
+        }
+
+        spaceCnt += spaceIncr;
+        this.print(node.right, spaceCnt);
+
+        console.log(
+            ' '.repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
+            `${node.data}`
+        );
+
+        this.print(node.left, spaceCnt);
+    }
+}
+
+const emptyTree = new BinarySearchTree();
+const oneNodeTree = new BinarySearchTree();
+oneNodeTree.root = new BSTNode(10);
+
+/* twoLevelTree
+        root
+        10
+      /   \
+    5     15
+*/
+const twoLevelTree = new BinarySearchTree();
+twoLevelTree.root = new BSTNode(10);
+twoLevelTree.root.left = new BSTNode(5);
+twoLevelTree.root.right = new BSTNode(15);
+
+/* fullTree
+                    root
+                <-- 25 -->
+              /            \
+            15             50
+          /    \         /    \
+        10     22      35     70
+      /   \   /  \    /  \   /  \
+    4    12  18  24  31  44 66  90
+*/
+/***************** Uncomment after insert method is created. ****************/
+// const fullTree = new BinarySearchTree();
+// fullTree
+//   .insert(25)
+//   .insert(15)
+//   .insert(10)
+//   .insert(22)
+//   .insert(4)
+//   .insert(12)
+//   .insert(18)
+//   .insert(24)
+//   .insert(50)
+//   .insert(35)
+//   .insert(70)
+//   .insert(31)
+//   .insert(44)
+//   .insert(66)
+//   .insert(90);
+
+// twoLevelTree.print();
+
+// console.log(twoLevelTree.max());
+console.log(twoLevelTree.maxRecursive());
+// console.log(twoLevelTree.min());
+console.log(twoLevelTree.minRecursive());
